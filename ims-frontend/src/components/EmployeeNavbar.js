560ffca2b107a0a10381cbdd55d0809500727a35
hasardulmi <hasardulmi@gmail.com>
2025-04-10 17:36:29 +0530

    sales for employee frontend is updated

--- ims-frontend/src/components/EmployeeNavbar.js
+++ ims-frontend/src/components/EmployeeNavbar.js
@@ -1,29 +1,20 @@
 // src/components/EmployeeNavbar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import LogoutIcon from '@mui/icons-material/Logout';
-
-function handleLogout() {
-    localStorage.removeItem('token');
-    sessionStorage.removeItem('user');
-    window.location.href = '/';
-}
+import { Link } from 'react-router-dom';
 
 const EmployeeNavbar = () => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" style={{ flexGrow: 1 }}>
-                    SARASI SHOE CORNER!
+                <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                    Employee Dashboard
                 </Typography>
-                <Button color="inherit" component={Link} to="/employee/dashboard">
-                    Dashboard
+                <Button color="inherit" component={Link} to="/employee-sales-report">
+                    Sales Report
                 </Button>
-                <Button color="inherit" component={Link} to="/profile">
-                    Profile
-                </Button>
-                <Button color="inherit" onClick={handleLogout} startIcon={<LogoutIcon />}>
+                {/* Add other navigation items as needed */}
+                <Button color="inherit" component={Link} to="/">
                     Logout
                 </Button>
             </Toolbar>
