678f125a402520a9bd82485e8a563b54666b2b6c
hasardulmi <hasardulmi@gmail.com>
2025-04-18 12:47:40 +0530

    update the ErrorResponse.java page

--- ims-backend/src/main/java/net/javaguides/ims_backend/dto/ErrorResponse.java
+++ ims-backend/src/main/java/net/javaguides/ims_backend/dto/ErrorResponse.java
@@ -1,7 +1,7 @@
-// src/main/java/net/javaguides/ims_backend/dto/ErrorResponse.java
 package net.javaguides.ims_backend.dto;
 
 public class ErrorResponse {
+
     private String message;
 
     public ErrorResponse(String message) {
